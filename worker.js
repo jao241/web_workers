6b9804f21aeedfcbfaa96d88c542bd668b676267
João Vitor Lima Lago Santos <vorque45@gmail.com>
2022-07-06 16:23:50 -0300

    onmessage movido para o topo do script

--- worker.js
+++ worker.js
@@ -1,12 +1,28 @@
+onmessage = function(message) {
+    // Dados enviados ao worker.
+    const { data } = message;
+
+    // Matriz contendo o resultado da multiplicação de matriz.
+    const matrizResultante = multiplicacaoParcialMatriz(data.parteMatrizA, data.matrizB);    
+
+    // Forma de chamar o metodo.
+    this.self.postMessage({matrizResultante, id: data.id});
+}
+
 // Realiza calculo parcial da multiplicação da matriz.
 function multiplicacaoParcialMatriz(elementoMatrizA, matrizB) {
+    // Armazena o somatório da multiplicação de linha da matriz A e a matriz B.
     let somatorioLinha = 0;
+    // Armazena o resultado final do calculo.
     let matrizResultante = [];
+    // Contador para a matriz resultante.
     let matrizResultanteCount = 0;
 
+    // Passando as linhas da matriz B.
     for(let countInterno = 0; countInterno < matrizB.length; countInterno++) {
         let linhaMatrizB = matrizB[countInterno];
 
+        // Calculo de linha.
         for(let countC = 0; countC < linhaMatrizB.length; countC++){
             somatorioLinha += elementoMatrizA[countC] * matrizB[countInterno][countC];
         }
@@ -16,11 +32,4 @@ function multiplicacaoParcialMatriz(elementoMatrizA, matrizB) {
     }
 
     return matrizResultante;
-}
-
-onmessage = function(message) {
-    const { data } = message;
-    const matrizResultante = multiplicacaoParcialMatriz(data.parteMatrizA, data.matrizB);    
-
-    this.self.postMessage({matrizResultante, id: data.id});
 }
